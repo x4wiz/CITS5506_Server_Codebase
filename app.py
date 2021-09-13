617149f8f4a29de746f0fd1fda6eed9b0e967def
John Pimenov <mail@23u.ru>
2021-09-13 17:17:06 +0800

    receiving the data added / displaying last line from the file

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template('main.html', title='Home')
 
 
 # Move to next lesson call
@@ -13,7 +13,18 @@ def hello_world():  # put application's code here
 def finish_lesson():
     response = request.get_json()
     print(response)
-    return jsonify(response)
+    with open('static/readings.txt', 'a') as file:
+        file.write("\n")
+        file.write(response["data"])
+    return response
+
+
+@app.route('/api/v1/get_readings', methods=["POST", "GET"])
+def get_readings():
+    with open('static/readings.txt', 'r') as file:
+        readings = file.readlines()
+
+    return readings[-1]
 
 
 if __name__ == '__main__':
