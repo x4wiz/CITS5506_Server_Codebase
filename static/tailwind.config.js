2e7da6439fdcd779f37249266724c938792a82b6
John Pimenov <mail@23u.ru>
2021-09-15 18:14:21 +0800

    changed requirements.txt

--- static/tailwind.config.js
+++ static/tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: [],
+  purge: [
+      './static/src/**/*.html',
+    './static/src/**/*.vue',
+    './static/src/**/*.jsx',
+      './templates/**/*.html',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
