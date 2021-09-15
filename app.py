2b3c7072bab088b0570c70defe0df5dc361e4681
John Pimenov <mail@23u.ru>
2021-09-15 18:07:13 +0800

    changing tailwind css settings, trying to purge unused styles

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask, request, render_template, jsonify
 
+from flask_assets import Bundle, Environment
+
 app = Flask(__name__)
+
+# Bundling src/main.css files into dist/main.css'
+css = Bundle('src/main.css', output='dist/main.css',
+             filters='postcss')
+
+assets = Environment(app)
+assets.register('main_css', css)
+css.build()
 
 
 @app.route('/')
